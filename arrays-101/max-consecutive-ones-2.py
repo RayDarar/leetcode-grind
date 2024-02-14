97d4caaafd47d6515e6ab81d08f8eab6f09b7131
RayDarar <dfqgth400@gmail.com>
2024-02-14 20:55:38 +0600

    finishing arrays-101

---
+++ arrays-101/max-consecutive-ones-2.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/max-consecutive-ones-ii/description/
+
+
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: list[int]) -> int:
+        max_count = 0
+        count = 0
+        post_count = 0
+
+        for i in range(0, len(nums)):
+            n = nums[i]
+
+            if n == 1:
+                count += 1
+                post_count += 1
+            elif n == 0:
+                count = post_count + 1
+                post_count = 0
+
+            if count > max_count:
+                max_count = count
+
+        return max_count
