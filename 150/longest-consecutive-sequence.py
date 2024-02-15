def46a722ff806283da7cbdae958757fb544d60c
RayDarar <dfqgth400@gmail.com>
2024-02-15 21:14:37 +0600

    150: longest-consecutive-sequence

--- 150/longest-consecutive-sequence.py
+++ 150/longest-consecutive-sequence.py
@@ -1,3 +1,20 @@
+# https://leetcode.com/problems/longest-consecutive-sequence/
+
 class Solution:
     def longestConsecutive(self, nums: list[int]) -> int:
-        pass
+        max_streak = 0
+        lookup_set = set(nums)
+
+        for n in lookup_set:
+            if n - 1 in lookup_set:
+                continue
+            next_n = n
+            streak = 1
+
+            while next_n + 1 in lookup_set:
+                next_n += 1
+                streak += 1
+
+            max_streak = max(max_streak, streak)
+
+        return max_streak
